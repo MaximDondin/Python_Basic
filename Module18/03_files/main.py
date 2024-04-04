10aca58586b26ad683f562427ace166945c00e4a
Serge K <ks7100313@gmail.com>
2024-04-04 22:13:36 +0300

    See comments

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -3,6 +3,8 @@ name = input('Название файла: ')
 
 if name.startswith(ban):
     print('Ошибка: название начинается на один из специальных символов.')
+    # TOOD: зачем дважды метод вызывать, когда в него можно сразу передать несколько аргументов?
+    # https://pythonz.net/references/named/str.startswith/
 elif not name.endswith('.txt') and not name.endswith('.docx'):
     print('Ошибка: неверное расширение файла. Ожидалось .txt или .docx.')
 else:
