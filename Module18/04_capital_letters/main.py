cded969d22c6af08ed76bd09eb7fc08b67e470d4
Serge K <ks7100313@gmail.com>
2024-04-05 21:49:31 +0300

    See comments

--- Module18/04_capital_letters/main.py
+++ Module18/04_capital_letters/main.py
@@ -3,3 +3,5 @@ text = input('Введите строку: ').split()
 print('\nРезультат:', end = ' ')
 for word in text:
     print(word.title(), end = ' ')
+
+# TODO: как работает .title() и почему не нужен цикл?
