0e80de54a207f5c12b2986b4b3ab01511bee6ac2
Serge K <ks7100313@gmail.com>
2024-04-05 21:50:59 +0300

    See comments

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -2,6 +2,8 @@ count = 0
 flag = False
 while True:
     password = input('Придумайте пароль: ')
+    # TODO: работаем в рамках пройденного материала, пожалуйста
+    # map() сам Гвидо не рекомендует использовать, а в таких случаях и подавно
     if sum(map(str.isnumeric, password)) >= 3 and sum(map(str.isupper, password)) >= 1 and len(password) >= 8:
         print('Это надёжный пароль!')
         break
