f9ae7594899f8891f4f53a1383f301163b82d381
Max <maks.dondin@yandex.ru>
2024-05-10 23:21:19 +0300

    error correction homework - 20

--- Module20/05_sort_function/main.py
+++ Module20/05_sort_function/main.py
@@ -1,8 +1,22 @@
 def tpl_sort(data):
+    data = list(data)
+    transform(data)
     for i_numb in data:
-        if not i_numb.is_integer():
-            return data
+        if type(i_numb) != int:
+            return tuple(data)
     return tuple(sorted(data))
+def transform(data):
+    for i_index, i_numb in enumerate(data):
+        if type(i_numb) == list or type(i_numb) == set:
+            new_numb = list(i_numb)
+            for j_index, j_numb in enumerate(new_numb):
+                data.insert(i_index + j_index, j_numb)
+            data.remove(i_numb)
+    return data
 
-# tpl = (6, 3, -1, 8, 4.5, 10, -5)
-# print(tpl_sort(tpl))
+print(tpl_sort((6, 3, -1, 8, 4, 10, -5)))
+print(tpl_sort((6, 3, -1, 8, 4, 10, -5, 'j')))
+print(tpl_sort((6, 3, -1, 8, 4.2, 10, -5)))
+print(tpl_sort((6, 3, -1, 8, [4], 10, -5)))
+print(tpl_sort((6, 3, -1, 8, [4, 10], -5)))
+print(tpl_sort((6, 3, -1, 8, {4, 7}, 10, -5)))
