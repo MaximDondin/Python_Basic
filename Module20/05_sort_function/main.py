953dd928577fd0b38a279a553520c67291e88a99
Максим Дондин <maks.dondin@yandex.ru>
2024-05-11 11:31:57 +0300

    Update_5 main.py

--- Module20/05_sort_function/main.py
+++ Module20/05_sort_function/main.py
@@ -1,18 +1,8 @@
 def tpl_sort(data):
-    data = list(data)
-    transform(data)
     for i_numb in data:
         if type(i_numb) != int:
             return tuple(data)
     return tuple(sorted(data))
-def transform(data):
-    for i_index, i_numb in enumerate(data):
-        if type(i_numb) == list or type(i_numb) == set:
-            new_numb = list(i_numb)
-            for j_index, j_numb in enumerate(new_numb):
-                data.insert(i_index + j_index, j_numb)
-            data.remove(i_numb)
-    return data
 
 print(tpl_sort((6, 3, -1, 8, 4, 10, -5)))
 print(tpl_sort((6, 3, -1, 8, 4, 10, -5, 'j')))
