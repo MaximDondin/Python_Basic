7111e66190fec26b2ac15d38275d9a09d44ae3a9
Максим Дондин <maks.dondin@yandex.ru>
2024-12-09 06:49:49 +0300

    work on bugs #4

--- Module31/04_phone_numbers/main.py
+++ Module31/04_phone_numbers/main.py
@@ -1,20 +1,19 @@
 import re
-from num2words import num2words
 
 if __name__ == '__main__':
     number_list = ['9999999999', '999999-999', '99999x9999', '9234567890', '0123456789']
-    # my_num2words = ['первый', 'второй', 'третий', 'четвертый', 'пятый']
+    my_num2words = ['первый', 'второй', 'третий', 'четвертый', 'пятый']
     pattern = r'\b[89]\d{9}\b'
 
     count = 0
     for number in number_list:
         result = re.findall(pattern, number)
         if result:
-            # print(f'{my_num2words[count]} номер ({number_list[count]}): все в порядке')
-            print(f'{num2words(count + 1, to='ordinal', lang='ru')} номер ({number_list[count]}): все в порядке')
+            print(f'{my_num2words[count]} номер ({number_list[count]}): все в порядке')
+            # print(f'{num2words(count + 1, to='ordinal', lang='ru')} номер ({number_list[count]}): все в порядке')
         else:
-            # print(f'{my_num2words[count]} номер ({number_list[count]}): не подходит')
-            print(f'{num2words(count + 1, to='ordinal', lang='ru')} номер ({number_list[count]}): не подходит')
+            print(f'{my_num2words[count]} номер ({number_list[count]}): не подходит')
+            # print(f'{num2words(count + 1, to='ordinal', lang='ru')} номер ({number_list[count]}): не подходит')
         count += 1
 
 else:
